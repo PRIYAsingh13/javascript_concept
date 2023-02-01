b278ee7da8bfdab614f767283acc4aef2b8cd4b3
PRIYAsingh13 <ps597934@gmail.com>
2023-02-01 21:15:06 +0530

    js

---
+++ index.js
@@ -0,0 +1,20 @@
+// ============== 1 ======
+
+// let a = [];
+// let b = [];
+// console.log(a==b);
+// console.log(a===b);
+
+// ======= 2 =======
+// let a = []
+// let b = a;
+// console.log(a == b);
+// console.log( a === b );
+
+// same memory locatin+++++
+
+// ======== 3 =======
+let a = [20];
+let b = [20];
+console.log(a[0] == b[0])
+console.log(a[0] === b[0]);
